72b000d7a4ac4c2bfa16ee220a6c4949fc22af0a
kotlin <kotlinken@gmail.com>
2022-04-10 02:18:24 +0900

    read ln function test with vcs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    val name = readln()
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
+    println("Hello, $name")
 }
